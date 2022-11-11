4aafe68928094b2169e946cabd1c057853b58563
Brunnosama <apbrunno@gmail.com>
2022-11-11 05:54:08 -0300

    chore: formated date & get sign date values

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,27 @@
-const getFormatedDate = () => {
-    const result = new Date (document.getElementById('input-date').value);
-    document.getElementById('formated-date').innerHTML = result.toLocaleDateString("pt-BR", {timeZone: 'UTC'})
-}; 
+const jsonData = 'signos.json';
 
-console.log('data de nascimento', getFormatedDate);
+const getInputedDate = () => {
+    const result = new Date(document.getElementById('input-date').value);
+    const day = result.getUTCDate();
+    const month = result.getUTCMonth() + 1;
+    const newDate = `${day}/${month}`;
+
+    document.getElementById('formated-date').innerHTML = newDate;
+
+};
+
+const getSign = async () => {
+    const response = await fetch(jsonData)
+    const data = await response.json();
+    const signData = data.signos;
+    const outputStart = signData.map(startDate => startDate.data_inicio);
+    const outputEnd = signData.map(endDate => endDate.data_fim);
+    console.log(outputStart, outputEnd);
+}
+
+document.getElementById('btn-sign').addEventListener('click', getInputedDate, getSign);
+document.getElementById('btn-sign').addEventListener('click', getSign);
+
 
 const inputDateEl = document.querySelector('#input-date')
 IMask(inputDateEl, {
