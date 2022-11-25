8bf8e679f6d1a0746825bd4d12e387faf9634373
Brunnosama <apbrunno@gmail.com>
2022-11-25 16:44:57 -0300

    chore: optimize code to map the json file

--- assets/js/getSign.js
+++ assets/js/getSign.js
@@ -6,38 +6,18 @@ const getSign = (date) => {
 
     let resultSigno = ''
 
-    let day = '' + date.getUTCDate();
-    if (day.length < 2) day = '0' + day;
+    let day = '' + date.getUTCDate(); if (day.length < 2) day = '0' + day;
     const inputDay = day;
 
-    let month = '' + (date.getUTCMonth() + 1);
-    if (month.length < 2) month = '0' + month;
+    let month = '' + (date.getUTCMonth() + 1); if (month.length < 2) month = '0' + month;
     const inputMonth = month;
 
-    if ((inputMonth == 03 && inputDay >= 21) || (inputMonth == 04 && inputDay <= 20)) { resultSigno = signData[0] }
-    else if (
-        (inputMonth == 04 && inputDay >= 21) || (inputMonth == 05 && inputDay <= 20)) { resultSigno = signData[1] }
-    else if (
-        (inputMonth == 05 && inputDay >= 21) || (inputMonth == 06 && inputDay <= 20)) { resultSigno = signData[2] }
-    else if (
-        (inputMonth == 06 && inputDay >= 22) || (inputMonth == 07 && inputDay <= 22)) { resultSigno = signData[3] }
-    else if (
-        (inputMonth == 07 && inputDay >= 23) || (inputMonth == 08 && inputDay <= 23)) { resultSigno = signData[4] }
-    else if (
-        (inputMonth == 08 && inputDay >= 24) || (inputMonth == 09 && inputDay <= 23)) { resultSigno = signData[5] }
-    else if (
-        (inputMonth == 09 && inputDay >= 24) || (inputMonth == 10 && inputDay <= 23)) { resultSigno = signData[6] }
-    else if (
-        (inputMonth == 10 && inputDay >= 24) || (inputMonth == 11 && inputDay <= 22)) { resultSigno = signData[7] }
-    else if (
-        (inputMonth == 11 && inputDay >= 23) || (inputMonth == 12 && inputDay <= 21)) { resultSigno = signData[8] }
-    else if (
-        (inputMonth == 12 && inputDay >= 22) || (inputMonth == 01 && inputDay <= 20)) { resultSigno = signData[9] }
-    else if (
-        (inputMonth == 01 && inputDay >= 21) || (inputMonth == 02 && inputDay <= 18)) { resultSigno = signData[10] }
-    else if (
-        (inputMonth == 02 && inputDay >= 19) || (inputMonth == 03 && inputDay <= 20)) { resultSigno = signData[11] }
+    for (let index in signData) {
+        const dataInicio = signData[index].data_inicio.split("/");
+        const dataFim = signData[index].data_fim.split("/");
 
+        if ((inputMonth == dataInicio[1] && inputDay >= dataInicio[0] || inputMonth == dataFim[1] && inputDay <= dataFim[0])) { resultSigno = signData[index] }
+    };
     return resultSigno;
 };
 
